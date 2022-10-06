4bbd1254d5559c1551eb2fc8512db54e59c93d8f
Alireza Milani <alireza.milani2011@gmail.com>
2022-10-06 16:42:21 +0330

    Set version for pre release lib

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -4,9 +4,9 @@
  */
 object ConfigData {
 
-    private const val versionMajor = 1
+    private const val versionMajor = 0
     private const val versionMinor = 0
-    private const val versionPatch = 0
+    private const val versionPatch = 1
 
     /**
      * Version code is made according below pattern
