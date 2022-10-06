23ec9c0e6a29b08cf6dcfd0a222c1c4f88a14bf7
Alireza Milani <alireza.milani2011@gmail.com>
2022-10-06 16:26:57 +0330

    Remove some .idea files

--- app/src/main/kotlin/com/alirezamilani/app/MainActivity.kt
+++ app/src/main/kotlin/com/alirezamilani/app/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.alirezamilani.app.ui.theme.PersianDateRangePickerTheme
 import com.alirezamilani.persiandaterangepicker.DateRangePicker
 
@@ -31,18 +28,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PersianDateRangePickerTheme {
-        Greeting("Android")
     }
 }
